8bf0e18835dd0a5e3b5adb8bb96216ce14c0ec04
Radames <radrivera@gmail.com>
2019-05-19 17:09:47 -0500

    added what info. users used to sign up

--- models/users.js
+++ models/users.js
@@ -1,33 +1,70 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var users = sequelize.define(
-    'users',
-    {
-      UserId: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
-      },
-      FirstName: DataTypes.STRING,
-      LastName: DataTypes.STRING,
-      Email: {
-        type: DataTypes.STRING,
-        unique: true
-      },
-      Username: {
-        type: DataTypes.STRING,
-        unique: true
-      },
-      Password: DataTypes.STRING,
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE
-    },
-    {}
-  );
-  users.associate = function(models) {
-    // associations can be defined here
-  };
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-  return users;
-};
+
+const userSchema = new Schema({
+
+
+  googleId: DataTypes.STRING,
+  FirstName: DataTypes.STRING,
+  LastName: DataTypes.STRING,
+  Email: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  Username: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  Password: DataTypes.STRING,
+  createdAt: DataTypes.DATE,
+  updatedAt: DataTypes.DATE
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
+
+
+
+
+
+
+
+
+
+
+
+// 'use strict';
+// module.exports = (sequelize, DataTypes) => {
+//   var users = sequelize.define(
+//     'users',
+//     {
+//       UserId: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: DataTypes.INTEGER
+//       },
+//       FirstName: DataTypes.STRING,
+//       LastName: DataTypes.STRING,
+//       Email: {
+//         type: DataTypes.STRING,
+//         unique: true
+//       },
+//       Username: {
+//         type: DataTypes.STRING,
+//         unique: true
+//       },
+//       Password: DataTypes.STRING,
+//       createdAt: DataTypes.DATE,
+//       updatedAt: DataTypes.DATE
+//     },
+//     {}
+//   );
+//   users.associate = function(models) {
+//     // associations can be defined here
+//   };
+
+//   return users;
+// };
